ef8ec51727a562267eeb70e4f8a29c054362c908
nruffatto <nruffatto@carroll.edu>
2019-10-09 15:49:10 -0600

    began filling out the Block class

--- Mail_Carrier_Sibs/src/game/Block.java
+++ Mail_Carrier_Sibs/src/game/Block.java
@@ -1,5 +1,14 @@
 package game;
 
 public class Block {
-
+	public static final int SOLID = 0;
+	public static final int MAILBOX = 1;
+	public static final int NUMBER_OF_PROPERTIES = 2;
+	private boolean[] properties = new boolean[NUMBER_OF_PROPERTIES];
+	
+	private String imageName;
+	public Block(String imageFileName, boolean[] attributes) {
+		
+	}
+	
 }
