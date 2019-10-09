05204b408e09747f4d60842f7072e1ff139b55c1
nruffatto <nruffatto@carroll.edu>
2019-10-09 16:39:44 -0600

    Nathan continued fleshing out the block class

--- Mail_Carrier_Sibs/src/game/Block.java
+++ Mail_Carrier_Sibs/src/game/Block.java
@@ -1,14 +1,37 @@
 package game;
 
 public class Block {
+	
+/*
+ * I thought it would be good to store the properties of blocks in a boolean array. That way, when we want to add
+ * another property, all we need to do is change the way the files are read and written. We won't need to mess
+ * with the constructor at all.
+ */
 	public static final int SOLID = 0;
 	public static final int MAILBOX = 1;
-	public static final int NUMBER_OF_PROPERTIES = 2;
+	public static final int NUMBER_OF_PROPERTIES = 2; //this number should equal however many property ints are listed
+	//above it, so update every time a new property is added
 	private boolean[] properties = new boolean[NUMBER_OF_PROPERTIES];
 	
 	private String imageName;
 	public Block(String imageFileName, boolean[] attributes) {
+		for(int i = 0; i < attributes.length; i++) {
+			properties[i] = attributes[i];
+		}
 		
+		imageName = imageFileName;
 	}
 	
+	public String getImageFileName() {
+		return imageName;
+	}
+	
+	
+	/*
+	 * returns whether this block has the given attrubute. For example, if this block is solid - meaning that
+	 * properties[0] = true - then is(SOLID) will return true.
+	 */
+	public boolean is(int attribute) {
+		return properties[attribute];
+	}
 }
